ec17aebdaea770074f3d55247e94a3544a9a06f6
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2020-01-14 16:56:12 +0100

    Add files via upload
    Today, I try to understand the basic of JavaScript. I was very interesting to see how it works and the fact that finally, I can code something more interacting, more like Python.

---
+++ main.js
@@ -0,0 +1,51 @@
+/*   Index
+
+    let fonction
+    variable
+    commentaire
+    opérateur (surtout égale)
+    if and else
+    fonction
+
+
+
+
+
+
+*/
+
+
+
+let myVariable ;
+
+myVariable = "Bob";
+
+let myName = "Arnaud";
+
+// Ceci est un commentaire
+
+/*Ceci aussi
+est un commentaire.*/
+
+
+let myNumber = 10 ;
+myNumber === 9;
+// ends false with === 
+
+
+let iceCream = "Chocolate";
+if (iceCream === "Chocolate") {
+    alert("J'adore le chocolat !");
+} else {
+    alert("Ooooh, mais j'aurais préféré au chocolat !");
+}
+
+
+
+fonction multiply (num1, num2) {
+    let result = num1 * num2;
+    return result;
+}
+
+
+
