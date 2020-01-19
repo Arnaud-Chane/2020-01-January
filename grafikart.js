91df4fdd358129892830232c64ecdf680d74d855
Arnaud-Chane <59416180+Arnaud-Chane@users.noreply.github.com>
2020-01-19 22:40:23 +0100

    Add files via upload
    (2020.01.19) Aujourd'hui, beaucoup de recherche sur des SSGs et des recherches de formation/vidéos sur JavaScript.
    J'ai suivi et lu les cours de Grafikart sur JavaScript et ce qui est intéressant à apprendre et comprendre comment fonctionne les Frontend et Backend.

---
+++ grafikart.js
@@ -0,0 +1,19 @@
+var name = "Arnaud"
+var caracter = {
+    age: 26,
+    origin: "reunion",
+    interest: ["html", "css", "javascript", "python"]
+}
+
+
+caracter.age
+caracter.interest[2]
+
+var game = {
+    bethesda: {
+        ES: "Skyrim",
+        Fallout: "New Vegas"
+    }
+}
+
+game.bethesda.ES
